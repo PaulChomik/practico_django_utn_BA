d49af07aff399c0b733871e75a6e5ece01280ccd
Paul Chomik <paulfernandez10@hotmail.com>
2023-10-09 01:02:05 -0300

    se agrego la carpeta imagenes, footer y nav bar

--- sitio/consolas/views.py
+++ sitio/consolas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+"""
+def index(request):
+    return HttpResponse("Hola Mundo!")
+"""
+def index(request):
+    params = {}
+    params['nombre_sitio'] = 'consolas'
+    return render(request, 'index.html', params)
